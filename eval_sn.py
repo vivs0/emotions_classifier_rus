34c4cb4b126340a93a728e7145f66f03ff105234
LSM <commondore.ne@gmail.com>
2018-02-21 12:37:33 +0300

    Evaluation script

---
+++ eval_sn.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 19 12:36:22 2018
+
+@author: lsm
+"""
+excited_val = pd.read_csv('excited10.csv')
+calm_val = pd.read_csv('calm10.csv')
+angry_val = pd.read_csv('angry10.csv')
+sad_val = pd.read_csv('depressed10.csv')
+
+r_excited = excited_val.text.apply(ec.run_neural_network)
+r_calm = calm_val.text.apply(ec.run_neural_network)
+r_angry = angry_val.text.apply(ec.run_neural_network)
+r_sad = sad_val.text.apply(ec.run_neural_network)
